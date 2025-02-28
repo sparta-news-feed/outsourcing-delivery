17b41b2b6b4c2d374b91309b3627bad28c1b85c3
이희수 <leehsdd33@gmail.com>
2025-02-28 19:48:24 +0900

    feat(menu): 메뉴 서비스 기초 세팅

---
+++ src/main/java/com/outsourcingdelivery/domain/menu/service/MenuService.java
@@ -0,0 +1,13 @@
+package com.outsourcingdelivery.domain.menu.service;
+
+import com.outsourcingdelivery.domain.menu.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MenuService {
+
+    private final MenuRepository menuRepository;
+    private final StoreRepository storeRepository;
+}
--- src/main/java/com/outsourcingdelivery/domain/menu/service/mock.java
+++
@@ -1,4 +0,0 @@
-package com.outsourcingdelivery.domain.menu.service;
-
-public class mock {
-}
