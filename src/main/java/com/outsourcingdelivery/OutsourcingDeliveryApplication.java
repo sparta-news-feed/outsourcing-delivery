449f05187f3019027ba8efa94ac10454e22360cb
JiHo Jeon <wjswlgh96@naver.com>
2025-02-28 17:02:37 +0900

    first commit

---
+++ src/main/java/com/outsourcingdelivery/OutsourcingDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.outsourcingdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsourcingDeliveryApplication.class, args);
+    }
+
+}
