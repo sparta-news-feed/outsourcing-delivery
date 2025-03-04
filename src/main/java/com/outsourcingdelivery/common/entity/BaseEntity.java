c6be2ada43138e5cc8ea93cf015b31443a3f4156
이현수 <mytime0316@naver.com>
2025-03-04 10:26:48 +0900

    choer:머지

--- src/main/java/com/outsourcingdelivery/common/entity/BaseEntity.java
+++ src/main/java/com/outsourcingdelivery/common/entity/BaseEntity.java
@@ -3,12 +3,15 @@ package com.outsourcingdelivery.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
@@ -20,5 +23,6 @@ public abstract class BaseEntity {
     @LastModifiedDate
     private LocalDateTime modifiedAt;
 
+    @Setter
     private LocalDateTime deletedAt;
 }
