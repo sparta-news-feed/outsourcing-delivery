451caaad64a25f6eb99c677d9b3c49427cc976ae
이현수 <mytime0316@naver.com>
2025-03-05 14:41:12 +0900

    fix: findByUser Query 수정

--- src/main/java/com/outsourcingdelivery/domain/store/repository/StoreRepository.java
+++ src/main/java/com/outsourcingdelivery/domain/store/repository/StoreRepository.java
@@ -14,7 +14,7 @@ public interface StoreRepository extends BaseRepository<Store, Long> {
     @Query("SELECT s FROM Store s WHERE s.deletedAt IS NULL")
     Page<Store> findAllPage(Pageable pageable);
 
-    @Query("SELECT s FROM Store s WHERE s.deletedAt IS NULL")
+    @Query("SELECT s FROM Store s WHERE s.user =:user AND s.deletedAt IS NULL")
     List<Store> findByUser(User user);
 
     Page<Store> findByStoreNameContainingIgnoreCase(String search, Pageable pageable);
