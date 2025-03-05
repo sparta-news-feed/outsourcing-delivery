5c290d7883978f5de568254864fd7300325de6f8
이희수 <leehsdd33@gmail.com>
2025-03-05 18:30:18 +0900

    refactor(menu): MenuSaveRequest DTO에 유효성 검사 어노테이션 추가 - 테스트코드 작성 중 발견한 유효성 검사 누락 부분 수정

--- src/main/java/com/outsourcingdelivery/domain/menu/dto/request/MenuSaveRequest.java
+++ src/main/java/com/outsourcingdelivery/domain/menu/dto/request/MenuSaveRequest.java
@@ -1,17 +1,20 @@
 package com.outsourcingdelivery.domain.menu.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class MenuSaveRequest {
-    @NotNull(message = "메뉴 이름 입력은 필수입니다.")
+    @NotBlank(message = "메뉴 이름 입력은 필수입니다.")
     @Size(max = 25, message = "메뉴 이름은 최대 25자까지 입력할 수 있습니다.")
     private String menuName;
 
     @NotNull(message = "메뉴 가격 입력은 필수입니다.")
+    @Positive(message = "가격은 양수여야 합니다.")
     private Integer price;
 
     @Size(max = 100, message = "설명은 최대 100자까지 입력할 수 있습니다.")
