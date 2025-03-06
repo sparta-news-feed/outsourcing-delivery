53d43eb8204c06c2e53f0d59d9ba4a83cc8cfc95
hanna <hannah.k0925@gmail.com>
2025-03-06 12:10:25 +0900

    test(order): 주문 생성 성공 테스트 추가

--- src/main/java/com/outsourcingdelivery/domain/order/dto/request/OrderCreateRequest.java
+++ src/main/java/com/outsourcingdelivery/domain/order/dto/request/OrderCreateRequest.java
@@ -2,6 +2,7 @@ package com.outsourcingdelivery.domain.order.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,4 +16,11 @@ public class OrderCreateRequest {
     @NotNull(message = "주문 수량은 필수값입니다.")
     @Min(1)
     private Integer amount;
+
+    @Builder
+    private OrderCreateRequest(Long storeId, Long menuId, Integer amount) {
+        this.storeId = storeId;
+        this.menuId = menuId;
+        this.amount = amount;
+    }
 }
--- src/test/java/com/outsourcingdelivery/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/outsourcingdelivery/domain/order/service/OrderServiceTest.java
@@ -1,17 +1,31 @@
 package com.outsourcingdelivery.domain.order.service;
 
+import com.outsourcingdelivery.common.config.PasswordEncoder;
+import com.outsourcingdelivery.common.dto.AuthUser;
+import com.outsourcingdelivery.common.exception.ErrorCode;
 import com.outsourcingdelivery.domain.SpringBootTestSupport;
 import com.outsourcingdelivery.domain.menu.entity.Menu;
 import com.outsourcingdelivery.domain.menu.repository.MenuRepository;
+import com.outsourcingdelivery.domain.order.dto.request.OrderCreateRequest;
+import com.outsourcingdelivery.domain.order.dto.response.OrderCreateResponse;
+import com.outsourcingdelivery.domain.order.entity.Order;
+import com.outsourcingdelivery.domain.order.enums.OrderStatus;
 import com.outsourcingdelivery.domain.order.repository.OrderRepository;
 import com.outsourcingdelivery.domain.store.entity.Store;
+import com.outsourcingdelivery.domain.store.enums.StoreStatus;
 import com.outsourcingdelivery.domain.store.repository.StoreRepository;
 import com.outsourcingdelivery.domain.user.entity.User;
+import com.outsourcingdelivery.domain.user.enums.UserType;
 import com.outsourcingdelivery.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
-//@Transactional
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
 class OrderServiceTest extends SpringBootTestSupport {
 
     @Autowired
@@ -29,12 +43,76 @@ class OrderServiceTest extends SpringBootTestSupport {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    private User owner;
     private User user;
-    private Menu menu;
     private Store store;
+    private Menu menu;
 
     @BeforeEach
     void setUp() {
+        user = User.builder()
+                .email("user@test.com")
+                .password(passwordEncoder.encode("Password1234!"))
+                .userType(UserType.USER)
+                .phoneNumber("01012345678")
+                .username("홍길동")
+                .build();
 
+        owner = User.builder()
+                .email("owner@test.com")
+                .password(passwordEncoder.encode("Password1234!"))
+                .userType(UserType.OWNER)
+                .phoneNumber("01012345678")
+                .username("홍길동")
+                .build();
+
+        store = Store.builder()
+                .storeName("가게명")
+                .minOrderPrice(10000)
+                .user(owner)
+                .build();
+
+        menu = Menu.builder()
+                .menuName("메뉴명")
+                .price(10000)
+                .description("메뉴설명")
+                .store(store)
+                .build();
+    }
+    
+    @Test
+    @DisplayName("주문 성공")
+    void createOrder1() throws Exception {
+        // given
+        User saveUser = userRepository.save(user);
+        User saveOwner = userRepository.save(owner);
+
+        Store saveStore = storeRepository.save(store);
+        saveStore.setStoreStatus(StoreStatus.OPEN);
+        storeRepository.save(saveStore);
+
+        Menu saveMenu = menuRepository.save(menu);
+
+        AuthUser authUser = AuthUser.builder()
+                .userId(saveUser.getUserId())
+                .build();
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .storeId(saveStore.getStoreId())
+                .menuId(saveMenu.getMenuId())
+                .amount(1)
+                .build();
+
+        // when
+        OrderCreateResponse response = orderService.createOrder(authUser, request);
+        Order order = orderRepository.findByIdOrElseThrow(response.getOrderNo(), ErrorCode.NOT_FOUND_ORDER);
+        
+        // then
+        assertThat(order)
+                .extracting("orderNo", "orderStatus", "amount", "user", "menu")
+                .containsExactly(response.getOrderNo(), OrderStatus.ORDERED, request.getAmount(), saveUser, saveMenu);
     }
 }
