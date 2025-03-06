264b7e1ee00dd807694af5fefa2985ed05c2834a
이현수 <mytime0316@naver.com>
2025-03-06 15:09:45 +0900

    test(store):콘트롤러 테스트 작성

---
+++ src/test/java/com/outsourcingdelivery/common/config/TestConfig.java
@@ -0,0 +1,20 @@
+package com.outsourcingdelivery.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class TestConfig {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
