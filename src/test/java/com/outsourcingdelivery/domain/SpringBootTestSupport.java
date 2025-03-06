82539e2fc2bc02e1592839abe286d5de9b8a5f55
JiHo Jeon <wjswlgh96@naver.com>
2025-03-06 15:08:17 +0900

    fix(test): import 제거

--- src/test/java/com/outsourcingdelivery/domain/SpringBootTestSupport.java
+++ src/test/java/com/outsourcingdelivery/domain/SpringBootTestSupport.java
@@ -1,8 +1,6 @@
 package com.outsourcingdelivery.domain;
 
-import com.outsourcingdelivery.common.config.TestConfig;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 public class SpringBootTestSupport {
